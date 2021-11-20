47349e3526c14ed3e5740e661b12189cd105a7c8
Lionandthelab <lionandthelab@gmail.com>
2021-11-21 01:41:01 +0900

    separate useAuth, /admin [patch for the previous commit]
    Signed-off-by: Lionandthelab <lionandthelab@gmail.com>

---
+++ src/pages/Admin.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {} from 'vue';
+import { firebaseUser, isSigned } from 'src/composables/useAuth';
+</script>
+<template>
+  <q-page padding>
+    <q-card
+      v-if="isSigned && firebaseUser?.email === 'raccoon.olive.829@example.com'"
+    >
+      관리자 페이지
+    </q-card>
+    <q-card v-else> 권한이 없습니다. </q-card>
+  </q-page>
+</template>
