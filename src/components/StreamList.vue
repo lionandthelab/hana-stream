4adadce61ca04c3f8103e9dc133b9277c026837e
Lionandthelab <lionandthelab@gmail.com>
2021-11-21 09:50:51 +0900

    fetch stream db and render
    Signed-off-by: Lionandthelab <lionandthelab@gmail.com>

--- src/components/StreamList.vue
+++ src/components/StreamList.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import {} from 'vue';
 import StreamListItem from 'src/components/StreamListItem.vue';
+import { db } from 'boot/firebase';
+import {
+  collection,
+  getDocs,
+  query,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from 'firebase/firestore';
+import { onMounted, ref } from 'vue';
+
+const items = ref<QueryDocumentSnapshot<DocumentData>[]>([]);
+
+const getData = async () => {
+  const q = query(collection(db, 'streams'));
+
+  const querySnapshot = await getDocs(q);
+  items.value = querySnapshot.docs;
+};
+
+onMounted(() => getData());
 </script>
 <template>
   <q-list>
-    <StreamListItem v-for="i in 7" :key="i" />
+    <StreamListItem v-for="item in items" :key="item.id" :item="item" />
   </q-list>
 </template>
--- src/components/StreamListItem.vue
+++ src/components/StreamListItem.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-import {} from 'vue';
+import { defineProps, computed } from 'vue';
+import { QueryDocumentSnapshot, DocumentData } from 'firebase/firestore';
+
+const props = defineProps<{
+  item: QueryDocumentSnapshot<DocumentData>;
+}>();
+
+const stream = computed(() => props.item.data());
 </script>
 <template>
-  <q-item> item </q-item>
+  <q-item> {{ stream }} </q-item>
 </template>
